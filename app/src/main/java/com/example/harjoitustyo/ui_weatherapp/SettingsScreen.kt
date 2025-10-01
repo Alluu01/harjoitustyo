2a8660855a8efb320bfbe2581b002e75b2129b9b
Alluu01 <aleksi.koppa@tuni.fi>
2025-10-01 15:57:03 +0300

    edit

--- app/src/main/java/com/example/harjoitustyo/ui_weatherapp/SettingsScreen.kt
+++ app/src/main/java/com/example/harjoitustyo/ui_weatherapp/SettingsScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
@@ -15,8 +16,14 @@ import androidx.navigation.NavHostController
 
 @Composable
 fun SettingsScreen() {
-    Column(modifier = Modifier.padding(16.dp)) {
-        Text("Settings Screen")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+     {
+        Text("Settings", style = MaterialTheme.typography.displaySmall)
         // Add toggle switches, preferences, TODOs, etc.
     }
 }
